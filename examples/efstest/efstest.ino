4e00020bd5197972f454a41fe29a77ccbb82f13e
Stefan <sl001@icloud.com>
2022-02-22 20:33:25 +0100

    basic test program

--- examples/efstest/efstest.ino
+++ examples/efstest/efstest.ino
@@ -3,8 +3,66 @@
 
 EepromFS EFS(0x50, 32768);
 
-void debug() {
-  Serial.print("Debug: "); Serial.println(EFS.debug);
+
+void formattest() {
+  Serial.println("Formating 32 slots");
+  long t=millis();
+  EFS.format(32);
+  t=millis()-t;
+  Serial.print("Format time = "); Serial.println(t);
+  Serial.print(16*32+6); Serial.println(" bytes updated");
+  Serial.print("Update baudrate = "); Serial.print((16*32+6)*8/t); Serial.println("000");
+
+ Serial.print("Error status: "); Serial.println(EFS.ferror);
+}
+
+void writetest(char* n, char* s) {
+  uint8_t f;
+  f=EFS.fopen(n, "w");
+  if (f) for(int i=0; i<256 && s[i]!=0; i++) EFS.fputc(s[i], f);
+  else Serial.println("Write error");
+
+  EFS.fflush(f);
+
+  Serial.println("Filesize now:");
+  Serial.println(EFS.filesize(f));
+
+  Serial.println("Bytes left in slot:");
+  Serial.println(EFS.available(f));
+  
+  EFS.fclose(f);
+
+  Serial.print("Error status: "); Serial.println(EFS.ferror);
+}
+
+void readtest(char *n) {
+  uint8_t f;
+  f=EFS.fopen(n, "r");
+
+  Serial.println("Bytes in file:");  
+  Serial.println(EFS.available(f));
+
+  if (f) for(int i=0; i<256 && ! EFS.eof(f); i++) Serial.write(EFS.fgetc(f));
+  else Serial.println("Read error");
+  Serial.println();
+  
+  EFS.fclose(f);
+
+  Serial.print("Error status: "); Serial.println(EFS.ferror);
+}
+
+void dirtest() {
+  EFS.dirp=0;
+  while (uint8_t f=EFS.readdir()) {
+          Serial.print("Slot "); 
+          Serial.print(f);
+          Serial.print(": ");
+          Serial.print(EFS.filename(f)); 
+          Serial.print(" "); 
+          Serial.println(EFS.filesize(f));
+   } 
+
+   Serial.print("Error status: "); Serial.println(EFS.ferror);
 }
 
 void setup() {
@@ -17,123 +75,73 @@ void setup() {
   // give Wire and Serial a little time
   delay(1000);
 
+  
   Serial.println("Filesystem test ");
 
   // initial format - begin doesn't do that 
-  Serial.println("Formating 32 slots");
-  long t=millis();
-  EFS.format(32);
-  t=millis()-t;
-  Serial.print("Format time = "); Serial.println(t);
-  Serial.print(16*32+6); Serial.println(" bytes updated");
-  Serial.print("Update baudrate * 1000 = "); Serial.print((16*32+6)*8/t); Serial.println();
-
+  Serial.println("-----------------------------------------");
+  formattest();
+  
   // begin the filesystem - return value is the number of slots in the FS
+  Serial.println("-----------------------------------------");
   int s;
   if (s=EFS.begin()) {
     Serial.println("Filessystem mounted");
     Serial.print(s); Serial.println(" slots in fs");
   }
+  dirtest();
 
-  // file io demo 
-  int file1=EFS.fopen("huhu", "w");  
-  Serial.println("File open for write:");
-  Serial.print("Slot id delivered "); Serial.println(file1);
+  Serial.println("-----------------------------------------");
+  Serial.println("Write test 1 - file1");
+  writetest("file1", "hello world");
+  readtest("file1");
 
-  Serial.println("Bytes in slot:");
-  Serial.println(EFS.available(file1));
-
-  char message[16]="hello world";
-  Serial.println("Storing to file:");
-  Serial.println(message);
+  Serial.println("-----------------------------------------");
+  Serial.println("Write test 2 - file1");
+  writetest("file1", "second write test");
+  readtest("file1");
+ 
+  Serial.println("-----------------------------------------");
+  Serial.println("Write test 3 - file2");
+  writetest("file2", "another file");
+  Serial.println("Directory now:");
+  dirtest();
+  Serial.println("Deleting file2");
+  EFS.remove("file2");
+  Serial.println("Directory now:");
+  dirtest();
   
-  for(int i=0; i<16 && message[i]!=0; i++) EFS.fputc(file1, message[i]);
+  Serial.println("-----------------------------------------");
 
-  Serial.println("Bytes left in slot:");
-  Serial.println(EFS.available(file1));
-
-  EFS.fclose(file1);
-
-  Serial.println("Filesize now:");
-  Serial.println(EFS.filesize(file1));
-
-  file1=EFS.fopen("huhu", "r");
-  Serial.println("File open for read:");
-  Serial.print("Slot id delivered "); Serial.println(file1);
-  
-  Serial.println("Reading content:");
-  while (!EFS.eof(file1)) Serial.write((char) EFS.fgetc(file1));
-  Serial.println();
-
-  Serial.println("Reading again:");
-  EFS.rewind(file1);
-  while (!EFS.eof(file1)) Serial.write((char) EFS.fgetc(file1));
-  Serial.println();
-
-  // directory access demo
-  int file2=EFS.fopen("haha", "w");  
-  Serial.println("Opened a second file for write");
-  Serial.print("Slot id delivered "); Serial.println(file2);
-  
-  EFS.fclose(file2);
-
-  EFS.dirp=0;
-  while (uint8_t file=EFS.readdir()) {
-          Serial.print("Slot "); 
-          Serial.print(file);
-          Serial.print(": ");
-          Serial.print(EFS.filename(file)); 
-          Serial.print(" "); 
-          Serial.println(EFS.filesize(file));
-   } 
-
-   EFS.remove("huhu");
-   Serial.println("Delete of file huhu");
-
-   EFS.dirp=0;
-   while (uint8_t file=EFS.readdir()) {
-        Serial.print("Slot "); 
-        Serial.print(file);
-        Serial.print(": ");
-        Serial.print(EFS.filename(file)); 
-        Serial.print(" "); 
-        Serial.println(EFS.filesize(file));
-   }
-
-  int file3=EFS.fopen("hihi", "w");  
+  int file3=EFS.fopen("file3", "w");  
   Serial.println("Opened a third file for write");
   Serial.print("Slot id delivered "); Serial.println(file3);
 
-  EFS.fflush(file3);
-
-  EFS.dirp=0;
-  while (uint8_t file=EFS.readdir()) {
-        Serial.print("Slot "); 
-        Serial.print(file);
-        Serial.print(": ");
-        Serial.print(EFS.filename(file)); 
-        Serial.print(" "); 
-        Serial.println(EFS.filesize(file));
+  for(int i=0; i<26; i++) {
+    EFS.fputc(65+i, file3); 
+    EFS.fputc(10, file3);
   }
-
-  EFS.rename("hihi", "holla");
-  Serial.println("Renamed a file");
-
-  EFS.dirp=0;
-  while (uint8_t file=EFS.readdir()) {
-        Serial.print("Slot "); 
-        Serial.print(file);
-        Serial.print(": ");
-        Serial.print(EFS.filename(file)); 
-        Serial.print(" "); 
-        Serial.println(EFS.filesize(file));
-  }
- 
-  Serial.println("-----------------------------------------");
   
+  EFS.fclose(file3);
+
+  dirtest();
+
+  int file1=EFS.fopen("file3", "r");
+
+  for (int i=0; !EFS.eof(file1); i++) {
+    char ch=EFS.fgetc(file1);
+    Serial.write(ch);
+  }
+
+  EFS.fclose(file1);
+
+  Serial.println("-----------------------------------------");
+
+  EFS.rename("file3", "file2");
+  Serial.println("Renamed a file");
+  dirtest();
+  Serial.println("-----------------------------------------");
 }
-
-
 
 void loop() {
   // put your main code here, to run repeatedly:
