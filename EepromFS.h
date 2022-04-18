6cf442d85de63215ca395b9db43e0ec9182c7c0d
Stefan <sl001@icloud.com>
2022-04-18 21:40:36 +0200

    Changed filename arguments to const char

--- EepromFS.h
+++ EepromFS.h
@@ -50,9 +50,9 @@ public:
 /*
 	POSIX style interface to the filesystem	
 */
-	uint8_t fopen(char* fn, char* m);
+	uint8_t fopen(const char* fn, const char* m);
 	uint8_t fclose(uint8_t f);
-	uint8_t fclose(char* m);
+	uint8_t fclose(const char* m);
 	bool eof(uint8_t f);
 	uint8_t fgetc(uint8_t f);
 	bool fputc(uint8_t ch, uint8_t f);
@@ -65,8 +65,8 @@ public:
 	uint8_t readdir();
 	char* filename(uint8_t f);
 	unsigned int filesize(uint8_t f);
-	uint8_t remove(char* fn);
-	uint8_t rename(char* ofn, char* nfn);
+	uint8_t remove(const char* fn);
+	uint8_t rename(const char* ofn, const char* nfn);
 
 /*
 	a few more methods
@@ -112,12 +112,12 @@ private:
 	unsigned int ifilesize;
 
 	// finding files and storage space
-	uint8_t findfile(char* fn);
+	uint8_t findfile(const char* fn);
 	uint8_t findemptyslot();
 	unsigned int findslot(uint8_t s);
 	void clearslotheader(uint8_t s);
 	uint8_t getfilename(uint8_t s);
-	void putfilename(uint8_t s, char* fn);
+	void putfilename(uint8_t s, const char* fn);
 	unsigned int getsize(uint8_t s);
 	void putsize(uint8_t s, unsigned int sz);
 	
